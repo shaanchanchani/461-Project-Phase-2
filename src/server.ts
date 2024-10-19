d446050d74d6ad7a7d2bfc4fb86a382c43ce528b
shaanchanchani <shaan.chan3@gmail.com>
2024-10-19 15:28:53 -0400

    removed server.ts

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';  // Use default import for express
-
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
